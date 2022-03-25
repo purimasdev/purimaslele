89405c14b08d2b475e3364ad6269486998c9d91f
dwisada <dwisadapt26@gmail.com>
2022-03-26 00:52:45 +0800

    update 2.2

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const express = require('express')
 const path = require('path')
 var compression = require('compression')
- 
+
 // ASSIGN CONST
 const app = express()
 const port = 3000
@@ -10,9 +10,8 @@ const port = 3000
 // GLOBAL VARIABLE
 app.locals.pml = {
   judul: 'Puri Mas Lele',
-  versi: '0.2.0',
+  versi: '0.2.2',
   icon: {
-    '48': '/icons/icon-48x48.png',
     '96': '/icons/icon-96x96.png',
   }
 }
@@ -28,10 +27,10 @@ app.use('/bulma', express.static(__dirname + '/node_modules/bulma/css/'));
 app.use('/icons', express.static(__dirname + '/node_modules/material-icons/iconfont'));
 
 // EXPRESS ROUTES
-app.get('/',(req,res) => res.render('home/index'))
+app.get('/', (req, res) => res.render('home/index'))
 
 // 404 Route
-app.get('*', (req,res) => {
+app.get('*', (req, res) => {
   res.sendStatus(404)
 })
 
