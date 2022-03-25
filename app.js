eaca965fa093b738f4a4371cfe11ec68224ebc26
dwisada <dwisadapt26@gmail.com>
2022-03-26 00:21:02 +0800

    homepage

--- app.js
+++ app.js
@@ -1,8 +1,39 @@
+// IMPORT MODULE
 const express = require('express')
+const path = require('path')
+var compression = require('compression')
+ 
+// ASSIGN CONST
 const app = express()
-
 const port = 3000
 
-app.get('/',(req,res) => res.send('Halo PuriMasLele'))
+// GLOBAL VARIABLE
+app.locals.pml = {
+  judul: 'Puri Mas Lele',
+  versi: '0.2.0',
+  icon: {
+    '48': '/icons/icon-48x48.png',
+    '96': '/icons/icon-96x96.png',
+  }
+}
 
+// EXPRESS MIDDLEWARE
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs')
+
+// EXPRESS STATIC FILE
+app.use(compression())
+app.use('/', express.static(path.join(__dirname, 'public')))
+app.use('/bulma', express.static(__dirname + '/node_modules/bulma/css/'));
+app.use('/icons', express.static(__dirname + '/node_modules/material-icons/iconfont'));
+
+// EXPRESS ROUTES
+app.get('/',(req,res) => res.render('home/index'))
+
+// 404 Route
+app.get('*', (req,res) => {
+  res.sendStatus(404)
+})
+
+// EXPRESS SERVER
 app.listen(port, () => console.log(`Server run on port : ${port}`))
